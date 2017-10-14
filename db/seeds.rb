5435f3ee5590c95a565acd93620acfc709e52b5c
Leandro Maioral <leandro.maioral@gmail.com>
2017-10-14 15:11:56 -0300

    Adding seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,109 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+question1 = Question.create!(
+  text: 'What is the 10th most spoken language worldwide?'
+)
+question1.answers.create!([
+  {
+    text: 'German',
+    correct: true
+  },
+  {
+    text: 'Bengali',
+    correct: false
+  },
+  {
+    text: 'Russian',
+    correct: false
+  },
+  {
+    text: 'Portuguese',
+    correct: false
+  },
+  {
+    text: 'Japanese',
+    correct: false
+  }
+])
+
+question2 = Question.create!(
+  text: 'What do you most fear in hormephobia?'
+)
+question2.answers.create!([
+  {
+    text: 'Saliva',
+    correct: false
+  },
+  {
+    text: 'Shock',
+    correct: true
+  },
+  {
+    text: 'Static',
+    correct: false
+  },
+  {
+    text: 'Silence',
+    correct: false
+  },
+  {
+    text: 'Blood',
+    correct: false
+  }
+])
+
+question3 = Question.create!(
+  text: 'Why was Charlie Brown feeling so depressed in A Charlie Brown Christmas?'
+)
+question3.answers.create!([
+  {
+    text: 'His friend was still in the psychiatric ward',
+    correct: false
+  },
+  {
+    text: 'Felt Christmas was being commercialized',
+    correct: true
+  },
+  {
+    text: 'Felt left out of a play he’d been asked to direct',
+    correct: false
+  },
+  {
+    text: 'Upset that his dog had been decorating without him',
+    correct: false
+  }
+])
+
+question4 = Question.create!(
+  text: "The word ‘chocolate’ originates from the word ‘xocolātl’ which means what?"
+)
+question4.answers.create!([
+  {
+    text: 'Bitter seed',
+    correct: false
+  },
+  {
+    text: 'Bitter plant',
+    correct: false
+  },
+  {
+    text: 'Bitter earth',
+    correct: false
+  },
+  {
+    text: 'Bitter water',
+    correct: true
+  }
+])
+
+question5 = Question.create!(
+  text: 'What’s the best way to stop crying while peeling onions?'
+)
+question5.answers.create!([
+  {
+    text: 'Suck lemons',
+    correct: false
+  },
+  {
+    text: 'Chew gum',
+    correct: true
+  }
+])
